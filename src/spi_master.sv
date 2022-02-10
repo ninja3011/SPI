8eb12d142caa0374b38eb2f7dc87dec10a8e8d6d
Ninad Jangle <ninadjangle3011@gmail.com>
2022-02-10 22:42:41 +0530

    Draft of SPI master

--- src/spi_master.sv
+++ src/spi_master.sv
@@ -1,1 +1,28 @@
-
+// Code your design here
+module spi_master(clk, mosi, miso, sck, nss, buffer,bufferReg, flag); //DUT code start
+  input  miso;
+  input  clk;
+  output reg  mosi;
+  output sck;
+  output  nss;
+  input reg [15:0] buffer;
+  
+  reg clear = 0;
+  output wire [15:0] bufferReg;
+  reg [15:0] shiftReg = 16'b10;
+  input reg [7:0 ]flag; 	
+always @(posedge clk )
+  begin
+    if(flag == 8'b0)begin
+    shiftReg = buffer;
+    end
+    if (clear)
+    shiftReg <= 16'b0;
+    else
+    shiftReg <= shiftReg >> 1;
+    shiftReg[15] <= miso;
+    mosi = shiftReg[0];
+    end
+assign bufferReg = shiftReg;
+  
+endmodule //DUT code end
